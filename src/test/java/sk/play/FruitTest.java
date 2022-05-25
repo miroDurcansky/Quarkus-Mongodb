0a2d70c2c814dc81d8caf69f2ac283cc2369c531
miroslav <durcansky.miroslav@gmail.com>
2022-05-25 23:27:38 +0200

    mongodb

---
+++ src/test/java/sk/play/FruitTest.java
@@ -0,0 +1,22 @@
+package sk.play;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+@QuarkusTest
+public class FruitTest {
+
+    @Inject
+    FruitService fruitService;
+
+    @Test
+    void addFruitInMongoDB(){
+        Fruit fruit = new Fruit();
+        fruit.setName("mango");
+        fruit.setDescription("mango is cool");
+
+        fruitService.add(fruit);
+    }
+}
